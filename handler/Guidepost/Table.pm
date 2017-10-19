49385216169249f69a7775e643c2e191f8e40a84
walley <walley@walley.org>
2017-10-19 15:11:34 +0200

    tags

--- handler/Guidepost/Table.pm
+++ handler/Guidepost/Table.pm
@@ -1436,10 +1436,34 @@ sub gp_line()
   $out .= "<script>\n";
   $out .= "\$('#ta" . $id . "').tagEditor({
 
-   autocomplete: { delay: 0, position: { collision: 'flip' }, source: ['infotabule', 'mapa', 'cyklo', 'ref', 'panorama', 'lyzarska', 'konska', 'rozcestnik', 'naucna', 'znaceni', 'zelena', 'cervena', 'zluta', 'modra', 'bila', 'rozmazane', 'necitelne', 'zastavka', 'memorial', 'eurodotace', 'historicke','smerovetabule'] },
+   autocomplete: { 
+     delay: 0, 
+     position: { collision: 'flip' }, 
+     source: [
+       'infotabule',
+       'mapa',
+       'cyklo',
+       'ref',
+       'panorama',
+       'lyzarska',
+       'konska',
+       'rozcestnik',
+       'naucna',
+       'znaceni',
+       'zelena', 'cervena', 'zluta', 'modra', 'bila',
+       'rozmazane',
+       'necitelne',
+       'zastavka',
+       'memorial',
+       'eurodotace',
+       'historicke',
+       'smerovatabule',
+       'vozickar',
+       'most'
+     ]
+   },
    placeholder: 'Vložte tagy ...',
    delimiter:';',
-
    onChange: function(field, editor, tags) {
    },
 
