53420ecca595e15ead7c1289ddafca4d87733e42
walley <walley@walley.org>
2021-09-09 11:22:02 +0200

    projectlist menu

---
+++ webapps/projectlist/menu.js
@@ -0,0 +1,51 @@
+function init_menu()
+{
+
+  $(function() {
+    $("#menu").menu();
+  });
+
+
+  $("#navmenu").click(function() {
+    $("#menu").toggle();
+  });
+
+
+  $("#navmenu").hover(
+    function() {
+      $(this).stop().animate({"border-color": "red"}, "slow");
+    },
+    function() {
+      $(this).stop().animate({"border-color": "black"}, "slow");
+    }
+  );
+
+  $("#menu").toggle();
+
+  get_username();
+
+}
+
+function set_username()
+{
+//  get_username();
+//alert ('username is '+username);
+  $("#username").html(username);
+}
+
+function get_username()
+{
+  var jqxhr;
+
+  $.ajaxSetup({xhrFields: { withCredentials: true } });
+  jqxhr= $.get("https://api.openstreetmap.social/table/username/")
+  .done(function(data) {
+    username = data;
+    set_username();
+  })
+  .fail(function() {
+    alert("username error");
+  })
+  .always(function() {
+  });
+}
